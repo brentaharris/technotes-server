33af1b5a5e55ea3e76699d9eb4c789dc7f8c202d
brentaharris <brentaustinharris@gmail.com>
2022-09-04 18:27:10 -0400

    added middleware and cors

---
+++ middleware/logger.js
@@ -0,0 +1,28 @@
+const { format } = require('date-fns')
+const { v4: uuid } = require('uuid')
+const fs = require('fs')
+const fsPromises = require('fs').promises
+const path = require('path')
+
+const logEvents = async (message, logFileName) => {
+    const dateTime = format(new Date(), 'yyyyMMdd\tHH:mm:ss')
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+
+    try {
+        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
+        }
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logFileName), logItem)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const logger = (req, res, next) => {
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
+    // todo add conditions to logging to file will not fill up too quickly
+    console.log(`${req.method} ${req.path}`)
+    next()
+}
+
+module.exports = { logEvents, logger }
--- server.js
+++ server.js
@@ -3,9 +3,25 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const { logger } = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookeParser = require('cookie-parser')
+const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
+
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(logger)
+
+app.use(cors(corsOptions))
+
+app.use(express.json())
+
+app.use(cookeParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+// can also use this way:
+// app.use(express.static('public'))
 
 app.use('/', require('./routes/root'))
 
@@ -20,6 +36,10 @@ app.all('*', (req, res) => {
     }
 })
 
+
+app.use(errorHandler)
+
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
 })
+
